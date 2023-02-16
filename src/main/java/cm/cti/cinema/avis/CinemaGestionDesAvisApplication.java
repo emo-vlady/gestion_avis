86e6810bbaef0816257d9a0512eda1d3f465e88e
vlady <emovlady1@gmail.com>
2023-02-16 10:47:22 +0100

    version principale

---
+++ src/main/java/cm/cti/cinema/avis/CinemaGestionDesAvisApplication.java
@@ -0,0 +1,13 @@
+package cm.cti.cinema.avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaGestionDesAvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaGestionDesAvisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/cti/cinema/avis/CinemaGestionDesAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.cti.cinema.avis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaGestionDesAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
